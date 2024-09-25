ce4c19a9a569c841d2aabf08fb2571078d57be68
Thushini Abeysuriya <thushiniabeysuriya@gmail.com>
2024-09-25 17:06:53 +0530

    Initial Commit

---
+++ IntroductionApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IntroductionApp
+//
+//  Created by Thushini Abeysuriya on 2024-09-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IntroductionApp/IntroductionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroductionAppApp.swift
+//  IntroductionApp
+//
+//  Created by Thushini Abeysuriya on 2024-09-25.
+//
+
+import SwiftUI
+
+@main
+struct IntroductionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
